80900b077477ea7a71bb18ce889d1b8e62374d9b
Czedrick <czedrickb1@learn.cssd.ab.ca>
2019-01-09 14:34:12 -0700

    nep nep

--- nep_wars/Assets/Scripts/Control.cs
+++ nep_wars/Assets/Scripts/Control.cs
@@ -14,8 +14,8 @@ public class Control : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-	
-		transform.position += move * speed * Time.deltaTime;
+
+		var movement = transform.position += move * speed * Time.deltaTime;
 
 	}
 }
