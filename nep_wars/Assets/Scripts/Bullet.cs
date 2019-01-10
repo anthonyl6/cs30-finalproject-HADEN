53cc9e4a2796d68da1b96b414dd7b58e1958fd92
Czedrick <czedrickb1@learn.cssd.ab.ca>
2019-01-10 14:36:03 -0700

    Bullet Wiggle

---
+++ nep_wars/Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	[SerializeField] float randomizer = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 Velocity = new Vector3(Random.Range(0f, randomizer), 0.0f);
+
+		transform.Translate(Velocity * Time.deltaTime);
+	}
+}
