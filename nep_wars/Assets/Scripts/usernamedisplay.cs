523b6f59273e46f99f28840b4adee99198d861f8
anthonyl6 <anthonyl6@learn.cssd.ab.ca>
2019-01-10 15:15:57 -0700

    tmpro fix

---
+++ nep_wars/Assets/Scripts/usernamedisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class usernamedisplay : MonoBehaviour {
+
+	//cached references
+	Login login;
+	TextMeshProUGUI myTextMeshProUGUI;
+
+	// Use this for initialization
+	void Start () {
+		login = FindObjectOfType<Login>();
+		myTextMeshProUGUI = GetComponent<TextMeshProUGUI>().text;
+		myTextMeshProUGUI = login.UsernameDisplay();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
