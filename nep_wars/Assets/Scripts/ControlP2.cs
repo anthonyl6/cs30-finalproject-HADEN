47a56cf204e733bb2873febeb9467a1a617b7e7e
Czedrick <czedrickb1@learn.cssd.ab.ca>
2019-01-11 13:42:46 -0700

    prinny dood

---
+++ nep_wars/Assets/Scripts/ControlP2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlP2 : MonoBehaviour {
+
+	float speed = 5.0f;
+
+
+
+	// Update is called once per frame
+	void Update () {
+		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+		var movement = transform.position += move * speed * Time.deltaTime;
+
+	}
+}
