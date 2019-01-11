f3c5a5f2255bcaef7609ab2ca576a64c112dada3
anthonyl6 <anthonyl6@learn.cssd.ab.ca>
2019-01-11 13:04:46 -0700

    Fixed userdisplay

---
+++ nep_wars/Assets/Scripts/usernamedisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class usernamedisplay : MonoBehaviour {
+
+	[SerializeField] TextMeshPro usernameText;
+
+	//cached references
+	Login login;
+	void Start()
+	{
+		login = FindObjectOfType<Login>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		usernameText.text = login.UsernameDisplay();
+	}
+}
