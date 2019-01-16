4d0a6f28ee8e0af70584aedf46aa660733683cd1
Czedrick <czedrickb1@learn.cssd.ab.ca>
2019-01-16 14:15:09 -0700

    player 2 shooter

---
+++ nep_wars/Assets/Scripts/ShootP2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootP2 : MonoBehaviour {
+
+
+	ObjectPool objectPool;
+
+	private void Start()
+	{
+		objectPool = ObjectPool.Instance;
+	}
+
+	void FixedUpdate () {
+
+		if(Input.GetKeyDown(KeyCode.P)) {
+			ObjectPool.Instance.SpawnFromPool("Cube", transform.position, Quaternion.identity);
+		}
+			
+	}
+	
+}
