8be40401abfc1e7b5b230dc7187bc58b45572c7b
anthonyl6 <anthonyl6@learn.cssd.ab.ca>
2019-01-17 15:09:10 -0700

    Added a character select screen.

---
+++ nep_wars/Assets/Scripts/SelectP1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectP1 : MonoBehaviour {
+
+	//cached references
+	[SerializeField] GameObject Chicken;
+	[SerializeField] GameObject Penguin;
+
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Horizontal2") < 0)
+		{
+			GetComponent<Transform>().position = new Vector2(Chicken.transform.position.x, Chicken.transform.position.y);
+		}
+		if(Input.GetAxis("Horizontal2") > 0)
+		{
+			GetComponent<Transform>().position = new Vector2(Penguin.transform.position.x, Penguin.transform.position.y);
+		}
+	}
+}
