eb97a8a3f3397f0bbd2750dcb76a7411dbd318ef
JullianP1 <jullianp1@learn.cssd.ab.ca>
2019-01-17 15:09:56 -0700

    fuck
    merge
    
    oof

--- nep_wars/Assets/Scripts/TextHover.cs
+++ nep_wars/Assets/Scripts/TextHover.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class TextHover : MonoBehaviour {
 
-	// Use this for initialization
+
+
 	void Start () {
-		
+		BoxCollider2D select = gameObject.GetComponent<BoxCollider2D>();
+		Sprite multiplayer = Resources.Load("text_multiplayer", typeof(Sprite)) as Sprite;
+		Sprite options = Resources.Load("text_options", typeof(Sprite)) as Sprite;
+		Sprite exit = Resources.Load("text_exit", typeof(Sprite)) as Sprite;
+	//---------
+		Sprite multiplayerHover = Resources.Load("text_multiplayer_hover", typeof(Sprite)) as Sprite;
+		Sprite optionsHover = Resources.Load("text_options_hover", typeof(Sprite)) as Sprite;
+		Sprite exitHover = Resources.Load("text_exit_hover", typeof(Sprite)) as Sprite;
+	}
+
+	void OnMouseOver() {
+		Debug.Log("ur over haha");
 	}
 	
 	// Update is called once per frame
