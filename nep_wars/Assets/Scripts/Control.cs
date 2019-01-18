b1eb621c06ccf681f85bca57bf6840d9edb8387d
Czedrick <czedrickb1@learn.cssd.ab.ca>
2019-01-18 13:29:28 -0700

    Boost on p1

--- nep_wars/Assets/Scripts/Control.cs
+++ nep_wars/Assets/Scripts/Control.cs
@@ -11,8 +11,13 @@ public class Control : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		var move = new Vector3(Input.GetAxis("Horizontal1"), Input.GetAxis("Vertical1"), 0);
-
 		var movement = transform.position += move * speed * Time.deltaTime;
+
+		if(Input.GetButton("Boost1")) {
+			speed = 10f;
+		} else {
+			speed = 5f;
+		}
 
 	}
 }
