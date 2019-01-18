c727e09d2b9c04abb8f31b802e68179ad54ed0d0
anthonyl6 <anthonyl6@learn.cssd.ab.ca>
2019-01-18 13:43:40 -0700

    merge

--- nep_wars/Assets/Scripts/SelectP1.cs
+++ nep_wars/Assets/Scripts/SelectP1.cs
@@ -8,16 +8,46 @@ public class SelectP1 : MonoBehaviour {
 	[SerializeField] GameObject Chicken;
 	[SerializeField] GameObject Penguin;
 
+	SelectP2 player2;
+
+	//variabled
+	int readyCheckP1;
+
+	void Start()
+	{
+		player2 = FindObjectOfType<SelectP2>();
+	}
 
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetAxis("Horizontal2") < 0)
+		SelectCharacter();
+	}
+
+	private void SelectCharacter()
+	{
+		if(Input.GetAxis("Horizontal1") < 0)
 		{
-			GetComponent<Transform>().position = new Vector2(Chicken.transform.position.x, Chicken.transform.position.y);
+			GetComponent<Transform>().position = new Vector3(Chicken.transform.position.x, Chicken.transform.position.y, Chicken.transform.position.z - 1);
 		}
-		if(Input.GetAxis("Horizontal2") > 0)
+		if(Input.GetAxis("Horizontal1") > 0)
 		{
-			GetComponent<Transform>().position = new Vector2(Penguin.transform.position.x, Penguin.transform.position.y);
+			GetComponent<Transform>().position = new Vector3(Penguin.transform.position.x, Penguin.transform.position.y, Chicken.transform.position.z - 1);
+		}
+		if(Input.GetAxis("Fire1") > 0)
+		{
+			readyCheckP1 = 1;
+			Ready();
+		}
+	}
+
+	private void Ready()
+	{
+		while(readyCheckP1 == 1)
+		{
+			if(Input.GetAxis("Boost1") > 0)
+			{
+				readyCheckP1 = 0;
+			}
 		}
 	}
 }
---
+++ nep_wars/Assets/Scripts/StartGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+	public void bothReady()
+	{
+		// check if both readyCheckP1 and readyCheckP2 equal eachother then goto the start scene.
+	}
+}
